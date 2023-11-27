01b2d7b5ca92561504e57074fe5c90a0a3867a35
hoavic <oceo.info@gmail.com>
2023-11-27 17:30:52 +0700

    Update web.php

--- packages/hoadev/coreblog/src/routes/web.php
+++ packages/hoadev/coreblog/src/routes/web.php
@@ -3,4 +3,13 @@
 use Hoadev\CoreBlog\Http\Controllers\CoreBlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('coreblog', CoreBlogController::class);
+Route::group([
+    'prefix' => 'admin'
+], function() {
+
+    Route::get('/', function() {
+        return 'active';
+    });
+    Route::get('coreblog', CoreBlogController::class);
+
+});
