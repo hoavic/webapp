a5a03564a3b5c75d8439e7b536b7f319b5ef6d78
hoavic <oceo.info@gmail.com>
2024-01-05 12:14:32 +0700

    5/1/2024:office:2

--- packages/hoadev/coreblog/src/views/layouts/blog/nav.blade.php
+++ packages/hoadev/coreblog/src/views/layouts/blog/nav.blade.php
@@ -1,7 +1,10 @@
-<ul class="max-w-7xl mx-auto my-0 w-full flex flex-col lg:flex-row gap-2 font-bold uppercase text-green-800">
-    <li><a href="{{ route('home') }}" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Home</a></li>
-    <li><a href="{{ route('admin.posts.index') }}" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Post</a></li>
-    <li><a href="{{ route('home') }}" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Phone</a></li>
-    <li><a href="{{ route('home') }}" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Contact</a></li>
+<ul class="max-w-7xl mx-auto my-0 w-full flex flex-col lg:flex-row font-bold uppercase text-green-800 whitespace-nowrap overflow-x-auto">
+    <li><a href="/gioi-thieu" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Giới thiệu</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Sản phẩm</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Tin tức</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Hướng dẫn mua hàng</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Hệ thống phân phối</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">FAQ</a></li>
+    <li><a href="#" class="py-2 px-4 block hover:bg-yellow-600 hover:text-white rounded-full transition-colors">Liên hệ</a></li>
 </ul>
 
